3b5966fa6470990e2ca2f546c22de65f24778f82
XuanThinhTu <tuthinh2004@gmail.com>
2025-02-11 18:11:34 +0700

    Raw log in form

--- fe_babyTracking/src/routes/index.jsx
+++ fe_babyTracking/src/routes/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "../App";
-import HomePage from "../pages/user/HomePage";
+import HomePage from "../pages/user/Home/HomePage";
+import SignIn from "../pages/user/Auth/Login";
+import UserHeader from "../components/header/UserHeader";
 
 function AppRouter() {
   return (
     <Router>
+      <UserHeader />
       <Routes>
-        <Route path="/test" element={<App />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<SignIn />} />
       </Routes>
     </Router>
   );
