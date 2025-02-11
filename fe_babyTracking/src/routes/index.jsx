0240007598f999eec5b2c78c78f9974537d79518
BuiDiepAnh <buidiepanh0905@gmail.com>
2025-02-11 09:49:40 +0700

    app routes

---
+++ fe_babyTracking/src/routes/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "../App";
+
+function AppRouter() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/test" element={<App />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
