c09bcdb0a38ee703508b8dfb452356fb17ecba80
XuanThinhTu <tuthinh2004@gmail.com>
2025-02-14 11:19:29 +0700

    resolve conflict

--- fe_babyTracking/src/routes/index.jsx
+++ fe_babyTracking/src/routes/index.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "../App";
-import AdminHomePage from "../pages/admin/AdminHomePage/AdminHomePage";
-import HomePage from "../pages/user/HomePage";
+import HomePage from "../pages/user/Home/HomePage";
+import SignIn from "../pages/user/Auth/Login";
+import UserHeader from "../components/header/UserHeader";
+import SignUp from "../pages/user/Auth/Register";
+import ForgotPassword from "../pages/user/Auth/ForgotPassword";
+import MainFooter from "../components/Footer";
 
 function AppRouter() {
   return (
     <Router>
-      <Routes>
-        <Route path="/admin" element={<AdminHomePage />} />
-        <Route path="/test" element={<App />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <UserHeader />
+      <div className="container mx-auto px-2 py-4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        </Routes>
+      </div>
+      <MainFooter />
+
     </Router>
   );
 }
