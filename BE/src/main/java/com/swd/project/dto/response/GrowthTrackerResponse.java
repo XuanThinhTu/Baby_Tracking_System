79792a63662582833211ce7e8956257d7f1b80a3
JAVA_10_HuynhTuanPhat <phathtse184816@fpt.edu.vn>
2025-02-18 17:01:41 +0700

    Create Predict Tracking data API

--- BE/src/main/java/com/swd/project/dto/response/GrowthTrackerResponse.java
+++ BE/src/main/java/com/swd/project/dto/response/GrowthTrackerResponse.java
@@ -1,8 +1,8 @@
 package com.swd.project.dto.response;
 
-import com.swd.project.entity.Children;
 import lombok.Builder;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Builder
@@ -12,7 +12,7 @@ public record GrowthTrackerResponse(
         Double weight,
         Double headCircumference,
         Double bmi,
-        Date measuredAt,
+        LocalDateTime measuredAt,
         int childrenId
 ) {
 }
