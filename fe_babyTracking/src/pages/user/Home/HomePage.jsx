dfd993709b4b3adb9f4c1451e333b95565d911bd
XuanThinhTu <tuthinh2004@gmail.com>
2025-03-01 17:24:33 +0700

    BMI Calculator in Homepage

--- fe_babyTracking/src/pages/user/Home/HomePage.jsx
+++ fe_babyTracking/src/pages/user/Home/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CarouselHero from '../../../components/Hero/CarouselHero'
 import MembershipPage from '../Membership/MembershipPage'
+import BMICalculator from './BMICalculator'
 
 function HomePage() {
     return (
         <>
             <CarouselHero />
+            <BMICalculator />
             <MembershipPage />
         </>
     )
