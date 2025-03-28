18fa773155b4e8fc6c646a9d693511aa121f93b3
JAVA_10_HuynhTuanPhat <phathtse184816@fpt.edu.vn>
2025-03-28 14:10:38 +0700

    fix booking

--- BE/src/main/java/com/swd/project/repository/BookingRepository.java
+++ BE/src/main/java/com/swd/project/repository/BookingRepository.java
@@ -11,10 +11,13 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
     boolean existsByDateAndDoctorAndSlotTime(LocalDate date, User doctor, SlotTime slotTime);
+
+    List<Booking> findByMemberOrDoctor(User member, User doctor);
 
     @Modifying
     @Transactional
